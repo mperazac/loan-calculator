953badce2c18174423938ac01afb0d8ba855c344
Mauricio Peraza <mauricio.peraza@revinate.com>
2023-04-06 19:26:29 -0600

    Replace tooltip component

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Tooltip } from 'flowbite-react';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import { Card as TremorCard, Text, Metric, Title } from '@tremor/react';
-import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 
 interface ICardProps {
   label: string;
@@ -25,15 +25,30 @@ const Card: React.FunctionComponent<ICardProps> = props => {
 
   return (
     <TremorCard decoration='top' decorationColor='indigo'>
-      <Title className='flex gap-2 items-center text-xs font-medium leading-none tracking-wider text-gray-500 uppercase dark:text-primary-light'>
-        {label}
-      </Title>
-      {tooltip && (
-        <Tooltip content={tooltip}>
-          <QuestionMarkCircleIcon className='h-5 w-5' />
-        </Tooltip>
-      )}
-      <Metric className='text-lg lg:text-3xl'>{value}</Metric>
+      <div className="flex gap-3">
+        <Title className='flex items-center gap-2 text-xs font-medium leading-none tracking-wider text-gray-500 uppercase dark:text-primary-light'>
+          {label}
+        </Title>
+        {tooltip && (
+          <Tooltip.Provider>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <InformationCircleIcon className='w-5 h-5' />
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  className='data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] bg-black text-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]'
+                  sideOffset={5}
+                >
+                  {tooltip}
+                  <Tooltip.Arrow className='fill-white' />
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        )}
+      </div>
+      <Metric className='text-lg xl:text-2xl'>{value}</Metric>
       {renderSubtext()}
     </TremorCard>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,34 @@
 module.exports = {
   content: [
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
-    './node_modules/flowbite-react/**/*.js',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  keyframes: {
+    slideDownAndFade: {
+      from: { opacity: 0, transform: 'translateY(-2px)' },
+      to: { opacity: 1, transform: 'translateY(0)' },
+    },
+    slideLeftAndFade: {
+      from: { opacity: 0, transform: 'translateX(2px)' },
+      to: { opacity: 1, transform: 'translateX(0)' },
+    },
+    slideUpAndFade: {
+      from: { opacity: 0, transform: 'translateY(2px)' },
+      to: { opacity: 1, transform: 'translateY(0)' },
+    },
+    slideRightAndFade: {
+      from: { opacity: 0, transform: 'translateX(2px)' },
+      to: { opacity: 1, transform: 'translateX(0)' },
+    },
+  },
+  animation: {
+    slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+    slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+    slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+    slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+  },
   theme: {
     extend: {},
   },
